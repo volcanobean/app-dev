09b4086eba9820e5aa4d36eb4a60a891032add93
Gene Kelly <volcanobean@gmail.com>
2015-01-11 23:04:12 -0600

    Disabled analytics in config file.

--- Wheres My Goblin/config.lua
+++ Wheres My Goblin/config.lua
@@ -14,5 +14,10 @@ application =
 			-- high-resolution devices (Retina iPad, Kindle Fire HD 9", Nexus 10, etc.) will use @2x-suffixed images
             -- devices less than 1200 pixels in width (iPhone 5, iPad 2, Kindle Fire 7", etc.) will use non-suffixed images
 		}
+	},
+	application =
+	{
+		-- Disable analytics, which are on by default
+	    launchPad = false,
 	}  
 }
